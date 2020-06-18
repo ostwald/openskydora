8c86d51b72efe61b087d08d16b28011f15b77e38
ostwald <ostwald@ucar.edu>
2020-06-18 22:16:37 +0000

    cleanup

--- theme/openskydora-in-collections.tpl.php
+++ theme/openskydora-in-collections.tpl.php
@@ -4,7 +4,11 @@
     <h2><?php print t('In collections'); ?></h2>
     <ul>
       <?php foreach ($parent_collections as $collection): ?>
-        <li><?php print l($collection->label, "islandora/object/{$collection->id}"); ?></li>
+      <li><?php print l($collection->label, "islandora/object/{$collection->id}", array (
+          'attributes' => array(
+              'class' => 'in-collection-link'
+          ),
+      )); ?></li>
       <?php endforeach; ?>
     </ul>
   </div>
